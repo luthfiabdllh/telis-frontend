cc360101031c6bc9ec86d79311f40c8701e546a4
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2026-07-07 10:24:01 +0700

    style: update file placeholder colors to use primary-foreground instead of primary

--- features/documents/components/file.tsx
+++ features/documents/components/file.tsx
@@ -37,26 +37,26 @@ const DefaultPlaceholder = () => {
   return (
     <div className="space-y-1.5">
       <div className="flex gap-2">
-        <div className="bg-primary/20 h-0.5 w-1/2 rounded-full" />
+        <div className="bg-primary-foreground/20 h-0.5 w-1/2 rounded-full" />
       </div>
       <div className="flex gap-1">
-        <div className="bg-primary/10 h-0.5 w-1/3 rounded-full" />
-        <div className="bg-primary/10 h-0.5 w-1/3 rounded-full" />
+        <div className="bg-primary-foreground/10 h-0.5 w-1/3 rounded-full" />
+        <div className="bg-primary-foreground/10 h-0.5 w-1/3 rounded-full" />
       </div>
       <div className="flex gap-1">
-        <div className="bg-primary/10 h-0.5 w-1/2 rounded-full" />
-        <div className="bg-primary/10 h-0.5 w-1/3 rounded-full" />
+        <div className="bg-primary-foreground/10 h-0.5 w-1/2 rounded-full" />
+        <div className="bg-primary-foreground/10 h-0.5 w-1/3 rounded-full" />
       </div>
       <div className="flex gap-1">
-        <div className="bg-primary/10 h-0.5 w-1/3 rounded-full" />
-        <div className="bg-primary/10 h-0.5 w-1/3 rounded-full" />
+        <div className="bg-primary-foreground/10 h-0.5 w-1/3 rounded-full" />
+        <div className="bg-primary-foreground/10 h-0.5 w-1/3 rounded-full" />
       </div>
       <div className="flex gap-1">
-        <div className="bg-primary/10 h-0.5 w-1/3 rounded-full" />
-        <div className="bg-primary/10 h-0.5 w-1/2 rounded-full" />
+        <div className="bg-primary-foreground/10 h-0.5 w-1/3 rounded-full" />
+        <div className="bg-primary-foreground/10 h-0.5 w-1/2 rounded-full" />
       </div>
       <div className="flex gap-1">
-        <div className="bg-primary/10 h-0.5 w-1/3 rounded-full" />
+        <div className="bg-primary-foreground/10 h-0.5 w-1/3 rounded-full" />
       </div>
     </div>
   );
@@ -101,17 +101,17 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
     filePlaceholder = (
       <div className="space-y-1.5">
         <div className="flex items-center gap-1">
-          <div className="text-primary/30 text-[10px] font-bold">#</div>
-          <div className="bg-primary/20 h-0.5 w-6 rounded-full" />
+          <div className="text-primary-foreground/30 text-[10px] font-bold">#</div>
+          <div className="bg-primary-foreground/20 h-0.5 w-6 rounded-full" />
         </div>
         <div className="space-y-1">
-          <div className="bg-primary/10 h-0.5 w-1/3 rounded-full" />
-          <div className="bg-primary/10 h-0.5 w-7 rounded-full" />
+          <div className="bg-primary-foreground/10 h-0.5 w-1/3 rounded-full" />
+          <div className="bg-primary-foreground/10 h-0.5 w-7 rounded-full" />
         </div>
         <div className="space-y-1">
-          <div className="bg-primary/10 h-0.5 w-8 rounded-full" />
-          <div className="bg-primary/10 h-0.5 w-4 rounded-full" />
-          <div className="bg-primary/10 h-0.5 w-1/3 rounded-full" />
+          <div className="bg-primary-foreground/10 h-0.5 w-8 rounded-full" />
+          <div className="bg-primary-foreground/10 h-0.5 w-4 rounded-full" />
+          <div className="bg-primary-foreground/10 h-0.5 w-1/3 rounded-full" />
         </div>
       </div>
     );
@@ -121,24 +121,24 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
     filePlaceholder = (
       <div className="space-y-0.5">
         <div className="grid grid-cols-3 gap-0.5">
-          <div className="bg-primary/20 h-2" />
-          <div className="bg-primary/20 h-2" />
-          <div className="bg-primary/20 h-2" />
+          <div className="bg-primary-foreground/20 h-2" />
+          <div className="bg-primary-foreground/20 h-2" />
+          <div className="bg-primary-foreground/20 h-2" />
         </div>
         <div className="grid grid-cols-3 gap-0.5">
-          <div className="bg-primary/5 h-2" />
-          <div className="bg-primary/5 h-2" />
-          <div className="bg-primary/5 h-2" />
-          <div className="bg-primary/5 h-2" />
-          <div className="bg-primary/5 h-2" />
-          <div className="bg-primary/5 h-2" />
+          <div className="bg-primary-foreground/5 h-2" />
+          <div className="bg-primary-foreground/5 h-2" />
+          <div className="bg-primary-foreground/5 h-2" />
+          <div className="bg-primary-foreground/5 h-2" />
+          <div className="bg-primary-foreground/5 h-2" />
+          <div className="bg-primary-foreground/5 h-2" />
         </div>
         <div className="grid grid-cols-3 gap-0.5">
-          <div className="bg-primary/5 h-2" />
-          <div className="bg-primary/5 h-2" />
+          <div className="bg-primary-foreground/5 h-2" />
+          <div className="bg-primary-foreground/5 h-2" />
         </div>
         <div className="grid grid-cols-3 gap-0.5">
-          <div className="bg-primary/5 h-2" />
+          <div className="bg-primary-foreground/5 h-2" />
         </div>
       </div>
     );
@@ -149,28 +149,28 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
       <>
         <div className="mb-2">
           <div className="grid grid-cols-3 gap-0.5">
-            <div className="bg-primary/20 h-1.5 rounded-full" />
-            <div className="bg-primary/20 h-1.5 rounded-full" />
-            <div className="bg-primary/20 h-1.5 rounded-full" />
+            <div className="bg-primary-foreground/20 h-1.5 rounded-full" />
+            <div className="bg-primary-foreground/20 h-1.5 rounded-full" />
+            <div className="bg-primary-foreground/20 h-1.5 rounded-full" />
           </div>
         </div>
         <div className="space-y-1.5">
           <div className="grid grid-cols-3 gap-0.5">
-            <div className="bg-primary/5 h-1 rounded-full" />
-            <div className="bg-primary/5 h-1 rounded-full" />
-            <div className="bg-primary/5 h-1 rounded-full" />
+            <div className="bg-primary-foreground/5 h-1 rounded-full" />
+            <div className="bg-primary-foreground/5 h-1 rounded-full" />
+            <div className="bg-primary-foreground/5 h-1 rounded-full" />
           </div>
           <div className="grid grid-cols-3 gap-0.5">
-            <div className="bg-primary/5 h-1 rounded-full" />
-            <div className="bg-primary/5 h-1 rounded-full" />
-            <div className="bg-primary/5 h-1 rounded-full" />
+            <div className="bg-primary-foreground/5 h-1 rounded-full" />
+            <div className="bg-primary-foreground/5 h-1 rounded-full" />
+            <div className="bg-primary-foreground/5 h-1 rounded-full" />
           </div>
           <div className="grid grid-cols-3 gap-0.5">
-            <div className="bg-primary/5 h-1 rounded-full" />
-            <div className="bg-primary/5 h-1 rounded-full" />
+            <div className="bg-primary-foreground/5 h-1 rounded-full" />
+            <div className="bg-primary-foreground/5 h-1 rounded-full" />
           </div>
           <div className="grid grid-cols-3 gap-0.5">
-            <div className="bg-primary/5 h-1 rounded-full" />
+            <div className="bg-primary-foreground/5 h-1 rounded-full" />
           </div>
         </div>
       </>
@@ -187,40 +187,40 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
       <div className="relative flex h-full flex-col items-center justify-center">
         <div className="space-y-0">
           <div className="flex overflow-hidden rounded-full">
-            <div className="bg-primary/20 size-1.5" />
-            <div className="bg-primary/5 size-1.5" />
+            <div className="bg-primary-foreground/20 size-1.5" />
+            <div className="bg-primary-foreground/5 size-1.5" />
           </div>
           <div className="flex overflow-hidden rounded-full">
-            <div className="bg-primary/5 size-1.5" />
-            <div className="bg-primary/20 size-1.5" />
+            <div className="bg-primary-foreground/5 size-1.5" />
+            <div className="bg-primary-foreground/20 size-1.5" />
           </div>
           <div className="flex overflow-hidden rounded-full">
-            <div className="bg-primary/20 size-1.5" />
-            <div className="bg-primary/5 size-1.5" />
+            <div className="bg-primary-foreground/20 size-1.5" />
+            <div className="bg-primary-foreground/5 size-1.5" />
           </div>
           <div className="flex overflow-hidden rounded-full">
-            <div className="bg-primary/5 size-1.5" />
-            <div className="bg-primary/20 size-1.5" />
+            <div className="bg-primary-foreground/5 size-1.5" />
+            <div className="bg-primary-foreground/20 size-1.5" />
           </div>
           <div className="flex overflow-hidden rounded-full">
-            <div className="bg-primary/20 size-1.5" />
-            <div className="bg-primary/5 size-1.5" />
+            <div className="bg-primary-foreground/20 size-1.5" />
+            <div className="bg-primary-foreground/5 size-1.5" />
           </div>
           <div className="flex overflow-hidden rounded-full">
-            <div className="bg-primary/5 size-1.5" />
-            <div className="bg-primary/20 size-1.5" />
+            <div className="bg-primary-foreground/5 size-1.5" />
+            <div className="bg-primary-foreground/20 size-1.5" />
           </div>
           <div className="flex overflow-hidden rounded-full">
-            <div className="bg-primary/20 size-1.5" />
-            <div className="bg-primary/5 size-1.5" />
+            <div className="bg-primary-foreground/20 size-1.5" />
+            <div className="bg-primary-foreground/5 size-1.5" />
           </div>
           <div className="flex overflow-hidden rounded-full">
-            <div className="bg-primary/5 size-1.5" />
-            <div className="bg-primary/20 size-1.5" />
+            <div className="bg-primary-foreground/5 size-1.5" />
+            <div className="bg-primary-foreground/20 size-1.5" />
           </div>
           <div className="flex overflow-hidden rounded-full">
-            <div className="bg-primary/20 size-1.5" />
-            <div className="bg-primary/5 size-1.5" />
+            <div className="bg-primary-foreground/20 size-1.5" />
+            <div className="bg-primary-foreground/5 size-1.5" />
           </div>
         </div>
       </div>
@@ -230,19 +230,19 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
   if (formatFile === "ppt" || formatFile === "pptx") {
     filePlaceholder = (
       <>
-        <div className="bg-primary/5 mb-1.5 space-y-1 rounded border p-1">
+        <div className="bg-primary-foreground/5 mb-1.5 space-y-1 rounded border p-1">
           <div className="flex justify-center gap-1">
             <div className="size-3 rounded-sm bg-orange-400/40" />
           </div>
-          <div className="bg-primary/15 mx-auto h-0.75 w-8 rounded-full" />
+          <div className="bg-primary-foreground/15 mx-auto h-0.75 w-8 rounded-full" />
         </div>
         <div className="mb-1 flex justify-center gap-1">
-          <div className="bg-primary/15 h-0.75 w-8 rounded-full" />
-          <div className="bg-primary/15 h-0.75 w-4 rounded-full" />
+          <div className="bg-primary-foreground/15 h-0.75 w-8 rounded-full" />
+          <div className="bg-primary-foreground/15 h-0.75 w-4 rounded-full" />
         </div>
         <div className="space-y-1">
-          <div className="bg-primary/15 h-0.75 w-4 rounded-full" />
-          <div className="bg-primary/15 h-0.75 w-5 rounded-full" />
+          <div className="bg-primary-foreground/15 h-0.75 w-4 rounded-full" />
+          <div className="bg-primary-foreground/15 h-0.75 w-5 rounded-full" />
         </div>
       </>
     );
@@ -256,12 +256,12 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
   ) {
     filePlaceholder = (
       <>
-        <div className="bg-primary/5 mb-1.5 space-y-1 rounded border p-1">
+        <div className="bg-primary-foreground/5 mb-1.5 space-y-1 rounded border p-1">
           <div className="flex justify-center gap-1">
             <div className="size-3 rounded-sm bg-yellow-400/40" />
           </div>
-          <div className="bg-primary/15 mx-auto mt-1 h-0.75 w-4 rounded-full" />
-          <div className="bg-primary/15 mx-auto h-0.75 w-8 rounded-full" />
+          <div className="bg-primary-foreground/15 mx-auto mt-1 h-0.75 w-4 rounded-full" />
+          <div className="bg-primary-foreground/15 mx-auto h-0.75 w-8 rounded-full" />
         </div>
       </>
     );
@@ -270,12 +270,12 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
   if (formatFile === "video") {
     filePlaceholder = (
       <>
-        <div className="bg-primary/5 mb-1.5 space-y-1 rounded border p-1">
+        <div className="bg-primary-foreground/5 mb-1.5 space-y-1 rounded border p-1">
           <div className="flex justify-center gap-1">
             <div className="size-0 border-y-[5px] border-l-8 border-y-transparent border-l-green-400/60" />
           </div>
-          <div className="bg-primary/15 mx-auto mt-1 h-0.75 w-4 rounded-full" />
-          <div className="bg-primary/15 mx-auto h-0.75 w-8 rounded-full" />
+          <div className="bg-primary-foreground/15 mx-auto mt-1 h-0.75 w-4 rounded-full" />
+          <div className="bg-primary-foreground/15 mx-auto h-0.75 w-8 rounded-full" />
         </div>
       </>
     );
@@ -291,24 +291,24 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
     filePlaceholder = (
       <div className="space-y-1">
         <div className="flex items-center gap-0.5">
-          <div className="text-primary/30 font-mono text-[5px]">&lt;</div>
+          <div className="text-primary-foreground/30 font-mono text-[5px]">&lt;</div>
           <div className="h-0.75 w-3 rounded-full bg-emerald-400/60" />
-          <div className="text-primary/30 font-mono text-[5px]">&gt;</div>
+          <div className="text-primary-foreground/30 font-mono text-[5px]">&gt;</div>
         </div>
         <div className="flex items-center gap-0.5 pl-1">
-          <div className="text-primary/30 font-mono text-[5px]">&lt;</div>
+          <div className="text-primary-foreground/30 font-mono text-[5px]">&lt;</div>
           <div className="h-0.75 w-2.5 rounded-full bg-sky-400/60" />
-          <div className="text-primary/30 font-mono text-[5px]">&gt;</div>
+          <div className="text-primary-foreground/30 font-mono text-[5px]">&gt;</div>
         </div>
         <div className="flex items-center gap-0.5 pl-1">
-          <div className="text-primary/30 font-mono text-[5px]">&lt;/</div>
+          <div className="text-primary-foreground/30 font-mono text-[5px]">&lt;/</div>
           <div className="h-0.75 w-2.5 rounded-full bg-sky-400/60" />
-          <div className="text-primary/30 font-mono text-[5px]">&gt;</div>
+          <div className="text-primary-foreground/30 font-mono text-[5px]">&gt;</div>
         </div>
         <div className="flex items-center gap-0.5">
-          <div className="text-primary/30 font-mono text-[5px]">&lt;</div>
+          <div className="text-primary-foreground/30 font-mono text-[5px]">&lt;</div>
           <div className="h-0.75 w-1 rounded-full bg-emerald-400/60" />
-          <div className="text-primary/30 font-mono text-[5px]">/&gt;</div>
+          <div className="text-primary-foreground/30 font-mono text-[5px]">/&gt;</div>
         </div>
       </div>
     );
@@ -318,7 +318,7 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
     filePlaceholder = (
       <div className="space-y-1">
         <div className="flex items-center gap-1">
-          <div className="text-primary/40 font-mono text-[6px]">{"{"}</div>
+          <div className="text-primary-foreground/40 font-mono text-[6px]">{"{"}</div>
         </div>
         <div className="flex items-center gap-1 pl-1.5">
           <div className="h-0.75 w-3 rounded-full bg-sky-400/60" />
@@ -333,7 +333,7 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
           <div className="h-0.75 w-4 rounded-full bg-sky-400/60" />
         </div>
         <div className="flex items-center gap-1">
-          <div className="text-primary/40 font-mono text-[6px]">{"}"}</div>
+          <div className="text-primary-foreground/40 font-mono text-[6px]">{"}"}</div>
         </div>
       </div>
     );
@@ -343,25 +343,25 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
     filePlaceholder = (
       <div className="space-y-1">
         <div className="flex items-center gap-1">
-          <div className="text-primary/40 font-mono text-[6px]">{"{"}</div>
+          <div className="text-primary-foreground/40 font-mono text-[6px]">{"{"}</div>
         </div>
         <div className="flex items-center gap-1 pl-1.5">
-          <div className="bg-primary/20 h-0.75 w-3 rounded-full" />
-          <div className="bg-primary/20 h-0.75 w-4 rounded-full" />
+          <div className="bg-primary-foreground/20 h-0.75 w-3 rounded-full" />
+          <div className="bg-primary-foreground/20 h-0.75 w-4 rounded-full" />
         </div>
         <div className="flex items-center gap-1 pl-1.5">
-          <div className="bg-primary/10 h-0.75 w-4 rounded-full" />
-          <div className="bg-primary/10 h-0.75 w-2 rounded-full" />
+          <div className="bg-primary-foreground/10 h-0.75 w-4 rounded-full" />
+          <div className="bg-primary-foreground/10 h-0.75 w-2 rounded-full" />
         </div>
         <div className="flex items-center gap-1 pl-1.5">
-          <div className="bg-primary/10 h-0.75 w-3 rounded-full" />
-          <div className="bg-primary/10 h-0.75 w-4 rounded-full" />
+          <div className="bg-primary-foreground/10 h-0.75 w-3 rounded-full" />
+          <div className="bg-primary-foreground/10 h-0.75 w-4 rounded-full" />
         </div>
         <div className="flex items-center gap-1 pl-1.5">
-          <div className="bg-primary/10 h-0.75 w-3 rounded-full" />
+          <div className="bg-primary-foreground/10 h-0.75 w-3 rounded-full" />
         </div>
         <div className="flex items-center gap-1">
-          <div className="text-primary/40 font-mono text-[6px]">{"}"}</div>
+          <div className="text-primary-foreground/40 font-mono text-[6px]">{"}"}</div>
         </div>
       </div>
     );
@@ -381,8 +381,8 @@ export const FileGraphic = ({ formatFile }: FileGraphicProps) => {
       </div>
       <div
         className={cn(
-          "relative z-1 space-y-3 rounded-md p-2 border border-primary/20",
-          "bg-primary/10 backdrop-blur-xl shadow-sm",
+          "relative z-1 space-y-3 rounded-md p-2 border border-primary/20 shadow-md overflow-hidden",
+          "bg-linear-to-b from-primary/90 to-primary/30 backdrop-blur-md",
           sizeClass
         )}
       >
